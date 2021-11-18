505c15e635409cc360ccb419f6958e2f6c86d1f9
lance.high <lance.high@hastings.edu>
2021-11-18 15:52:02 -0600

    Finidhed with assignment 3.2

---
+++ File Duplicator/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace File_Duplicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnSourceBrowse = new System.Windows.Forms.Button();
+            this.btnDestBrowse = new System.Windows.Forms.Button();
+            this.btnDuplicate = new System.Windows.Forms.Button();
+            this.ofdOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.sfdSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(12, 23);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(44, 13);
+            this.lblSource.TabIndex = 0;
+            this.lblSource.Text = "Source:";
+            // 
+            // lblDestination
+            // 
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(12, 52);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(63, 13);
+            this.lblDestination.TabIndex = 1;
+            this.lblDestination.Text = "Destination:";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(81, 49);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.ReadOnly = true;
+            this.txtDestination.Size = new System.Drawing.Size(525, 20);
+            this.txtDestination.TabIndex = 2;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(81, 20);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(525, 20);
+            this.txtSource.TabIndex = 3;
+            // 
+            // btnSourceBrowse
+            // 
+            this.btnSourceBrowse.Location = new System.Drawing.Point(611, 17);
+            this.btnSourceBrowse.Name = "btnSourceBrowse";
+            this.btnSourceBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnSourceBrowse.TabIndex = 4;
+            this.btnSourceBrowse.Text = "Browse";
+            this.btnSourceBrowse.UseVisualStyleBackColor = true;
+            this.btnSourceBrowse.Click += new System.EventHandler(this.btnSourceBrowse_Click);
+            // 
+            // btnDestBrowse
+            // 
+            this.btnDestBrowse.Location = new System.Drawing.Point(611, 46);
+            this.btnDestBrowse.Name = "btnDestBrowse";
+            this.btnDestBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnDestBrowse.TabIndex = 5;
+            this.btnDestBrowse.Text = "Browse";
+            this.btnDestBrowse.UseVisualStyleBackColor = true;
+            this.btnDestBrowse.Click += new System.EventHandler(this.btnDestBrowse_Click);
+            // 
+            // btnDuplicate
+            // 
+            this.btnDuplicate.Location = new System.Drawing.Point(308, 75);
+            this.btnDuplicate.Name = "btnDuplicate";
+            this.btnDuplicate.Size = new System.Drawing.Size(75, 23);
+            this.btnDuplicate.TabIndex = 6;
+            this.btnDuplicate.Text = "Duplicate";
+            this.btnDuplicate.UseVisualStyleBackColor = true;
+            this.btnDuplicate.Click += new System.EventHandler(this.btnDuplicate_Click);
+            // 
+            // ofdOpenFile
+            // 
+            this.ofdOpenFile.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 107);
+            this.Controls.Add(this.btnDuplicate);
+            this.Controls.Add(this.btnDestBrowse);
+            this.Controls.Add(this.btnSourceBrowse);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.lblSource);
+            this.Name = "Form1";
+            this.Text = "File Duplicator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnSourceBrowse;
+        private System.Windows.Forms.Button btnDestBrowse;
+        private System.Windows.Forms.Button btnDuplicate;
+        private System.Windows.Forms.OpenFileDialog ofdOpenFile;
+        private System.Windows.Forms.SaveFileDialog sfdSaveFile;
+    }
+}
+
---
+++ File Duplicator/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace File_Duplicator
+{
+    public partial class Form1 : Form
+    {
+        // Fields
+        // Keep track of the source file path
+        private string strSourcePath = null;
+        // Keep track of destination path
+        private string strDestPath = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSourceBrowse_Click(object sender, EventArgs e)
+        {
+            // Show the open file dialog and store the result it returns
+            DialogResult drResult = ofdOpenFile.ShowDialog();
+            // Only open and display the file if the user clicked open which returns a dialog result "OK"
+            if (drResult == DialogResult.OK)
+            {
+                // Display file path in textbox
+                txtSource.Text = ofdOpenFile.FileName;
+                strSourcePath = ofdOpenFile.FileName;
+            }
+        }
+
+        private void btnDestBrowse_Click(object sender, EventArgs e)
+        {
+            if (strSourcePath != null)
+            {
+                // Seperate the directory name from the file name
+                string strDirName = Path.GetDirectoryName(strSourcePath);
+                string strFileName = Path.GetFileName(strSourcePath);
+                sfdSaveFile.InitialDirectory = strDirName;
+                sfdSaveFile.FileName = strFileName;
+            }
+            // Only open and display the file if the user clicked open which returns a dialog result "OK"
+            if (sfdSaveFile.ShowDialog() == DialogResult.OK)
+            {
+                // Display file path in textbox
+                txtDestination.Text = sfdSaveFile.FileName;
+                strDestPath = sfdSaveFile.FileName;
+            }
+        }
+
+        private void btnDuplicate_Click(object sender, EventArgs e)
+        {
+            // Could use this, but assignment asks for the other way
+            // File.Copy(strSourcePath, strDestPath);
+
+            // Open source file to read
+            using (FileStream fsSourceFile = File.Open(strSourcePath, FileMode.Open, FileAccess.Read))
+            {
+                // Open/Create destination file to write 
+                using (FileStream fsDestFile = File.Open(strDestPath, FileMode.Create, FileAccess.Write))
+                {
+                    int iBytesRead;
+                    byte[] byBuffer = new byte[4096];
+                    while ((iBytesRead = fsSourceFile.Read(byBuffer, 0, 4096)) > 0)
+                    {
+                        fsDestFile.Write(byBuffer, 0, iBytesRead);
+                    }
+                }
+            }
+            MessageBox.Show("File has been duplicated.");
+        }
+    }
+}
